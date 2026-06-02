e251b4d79f05c547e5ef7fb96e7d284edbcc1d24
EricWimsatt <ericwimsatt@gmail.com>
2026-06-01 19:46:11 -0700

    oragni

--- uthCollusion/strategyBruteForce/run_simulation.py
+++ uthCollusion/strategyBruteForce/run_simulation.py
@@ -1,19 +1,46 @@
 from treys import Card, Deck, evaluation
 
-deck = Deck()
-player_hand = deck.pull_many([Card.new('Qh'), Card.new('5d')])
-deck.pull(Card.new('Ad'))
+def play_game(pre_flop_strategy, post_flop_strategy, river_strategy, dead_card_maker=lambda hand: []):
+    deck = Deck()
+    player_hand = deck.draw(2)
+    board = []
+    dead_cards = deck.pull_many(dead_card_maker(player_hand))
 
-Card.print_pretty_cards(player_hand)
-deck.sorted_print()
+    dealer_hand = deck.draw(2)
 
-for card in player_hand:
-    print(Card.get_rank_int(card), Card.get_suit_int(card), Card.int_to_pretty_str(card))
+    player_bet = 0
 
-quad_3s_value = evaluation.evaluate([Card.new('3h'), Card.new('3d')], [Card.new('2s'), Card.new('8h'), Card.new('2c')])
-print(quad_3s_value)
+    if pre_flop_strategy(player_hand, dead_cards):
+        player_bet = 4
 
-def check_strategy():
+    board += deck.draw(3)
+    if player_bet == 0 and  post_flop_strategy(player_hand, board, dead_cards):
+        player_bet = 2
+
+    board += deck.draw(2)
+    if player_bet == 0:
+        if river_strategy(player_hand, board, dead_cards):
+            player_bet = 1
+        else:
+            return -2
+
+    dealer_rank = evaluation.evaluate(dealer_hand, board)
+    player_rank = evaluation.evaluate(player_hand, board)
+
+    # Check if dealer qualifies
+    # 2860 is max rank for a pair
+    if dealer_rank > 2860:
+        wager = player_bet + 2
+    else:
+        wager = player_bet + 1
+
+    if player_rank < dealer_rank:
+        return wager
+    else:
+        return -wager
+
+
+def run_simulation(
     pass
 
 if __name__ == "__main__":
